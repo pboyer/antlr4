49586033733c5ebfec158d31f3a7ae999f9c5883
Peter Boyer <peter.b.boyer@gmail.com>
2016-10-05 11:24:48 -0400

    regen runtime tests

--- runtime-testsuite/test/org/antlr/v4/test/runtime/go/TestParseTrees.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/go/TestParseTrees.java
@@ -193,7 +193,7 @@ public class TestParseTrees extends BaseTest {
 	@Test
 	public void testTokenAndRuleContextString() throws Exception {
 		mkdir(parserpkgdir);
-		StringBuilder grammarBuilder = new StringBuilder(167);
+		StringBuilder grammarBuilder = new StringBuilder(195);
 		grammarBuilder.append("grammar T;\n");
 		grammarBuilder.append("s\n");
 		grammarBuilder.append("@init {\n");
@@ -204,7 +204,7 @@ public class TestParseTrees extends BaseTest {
 		grammarBuilder.append("}\n");
 		grammarBuilder.append("  : r=a ;\n");
 		grammarBuilder.append("a : 'x' { \n");
-		grammarBuilder.append("fmt.Println(p.GetRuleInvocationStack(nil))\n");
+		grammarBuilder.append("fmt.Println(antlr.PrintJavaStringArray(p.GetRuleInvocationStack(nil)))\n");
 		grammarBuilder.append("} ;");
 		String grammar = grammarBuilder.toString();
 		String input ="x";
